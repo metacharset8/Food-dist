bdb41ebd3aa7638de67cf900cfa1af2f8ef4d2c7
metacharset8 <iurasukhenko783@gmail.com>
2022-06-05 12:09:09 +0300

    menu feature added

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
 const tabCon = document.querySelectorAll(".tabcontent");
-const tabHeaderItems = document.querySelectorAll(".tabheader__item");
+const tabHeaderItem = document.querySelectorAll(".tabheader__item");
+const tabHeaderItems = document.querySelectorAll(".tabheader__items");
 
 function HideBlocks() {
   tabCon.forEach((element) => {
     element.classList.add("hide");
   });
 
-  tabHeaderItems.forEach((item) => {
+  tabHeaderItem.forEach((item) => {
     item.classList.remove("tabheader__item_active");
   });
 }
 
 function ShowBlocks(num = 0) {
   tabCon[num].classList.remove("hide");
-  tabCon[num].classList.add("show");
-  tabHeaderItems[num].classList.add("tabheader__item_active");
+  tabCon[num].classList.add("show", "fade");
+  tabHeaderItem[num].classList.add("tabheader__item_active");
 }
 
 HideBlocks();
 ShowBlocks();
+
+
+tabHeaderItems.forEach((items) => {
+  items.addEventListener("click", (e) => {
+    let targetItem = e.target;
+    for (let i = 0; i <= tabHeaderItem.length; i++) {
+      if (tabHeaderItem[i] == targetItem) {
+        HideBlocks();
+        ShowBlocks(i);
+      }
+    }
+  });
+});

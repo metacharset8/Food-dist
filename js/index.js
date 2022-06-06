0968c30786258da4ee50a154d35e371b7d83486c
metacharset8 <iurasukhenko783@gmail.com>
2022-06-06 13:52:03 +0300

    timer added

--- js/index.js
+++ js/index.js
@@ -21,7 +21,6 @@ function ShowBlocks(num = 0) {
 HideBlocks();
 ShowBlocks();
 
-
 tabHeaderItems.forEach((items) => {
   items.addEventListener("click", (e) => {
     let targetItem = e.target;
@@ -33,3 +32,49 @@ tabHeaderItems.forEach((items) => {
     }
   });
 });
+
+function getTimeRemainging(date) {
+  let stockEndDate = new Date(date);
+  let currentDate = new Date();
+  let gap = stockEndDate - currentDate;
+
+  let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+  let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
+  let minutes = Math.floor(gap / 1000 / 60) % 60;
+  let seconds = Math.floor(gap / 1000) % 60;
+
+  let time = {
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+    gap: gap,
+  };
+  return time;
+}
+
+function setClock() {
+  const setClockDays = document.getElementById("days");
+  const setClockHours = document.getElementById("hours");
+  const setClockMinutes = document.getElementById("minutes");
+  const setClockSeconds = document.getElementById("seconds");
+  let intervalCall = setInterval(updateClock, 1000);
+
+  function updateClock() {
+    let funcCall = getTimeRemainging("2022-06-20T00:00:00");
+    setClockDays.innerHTML = funcCall.days;
+    setClockHours.innerHTML = funcCall.hours;
+    setClockMinutes.innerHTML = funcCall.minutes;
+    setClockSeconds.innerHTML = funcCall.seconds;
+
+    if (funcCall.gap < 0) {
+      clearInterval(intervalCall);
+      setClockDays.innerHTML = 0;
+      setClockHours.innerHTML = 0;
+      setClockMinutes.innerHTML = 0;
+      setClockSeconds.innerHTML = 0;
+    }
+  }
+  updateClock();
+}
+setClock();
